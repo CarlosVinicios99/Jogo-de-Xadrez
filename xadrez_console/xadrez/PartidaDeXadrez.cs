5feb67340d9972588ccbe443dd3d840e79e2b963
Carlos Vinicios <carlosvinicios@id.uff.br>
2021-10-14 19:24:08 -0300

    Implementando Xequemate

--- xadrez_console/xadrez/PartidaDeXadrez.cs
+++ xadrez_console/xadrez/PartidaDeXadrez.cs
@@ -76,8 +76,46 @@ namespace xadrez
                 xeque = false;
             }
 
+            if(testeXequemate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+            }
+
             turno++;
             mudaJogador();
+        }
+
+        public bool testeXequemate(Cor cor)
+        {
+            if(estaEmXeque(cor))
+            {
+                return false;
+            }
+
+            foreach(Peca x in pecasEmJogo(cor))
+            {
+                bool[,] mat = x.movimentosPossiveis();
+                for(int i = 0; i < tab.linhas; i++)
+                {
+                    for(int j = 0; j < tab.colunas; j++)
+                    {
+                        if(mat[i, j])
+                        {
+                            Posicao origem = x.posicao;
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+
+                            if(!testeXeque)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         public void validarPosicaoDeOrigem(Posicao pos)
